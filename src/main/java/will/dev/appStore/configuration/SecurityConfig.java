9432612ff0159e1c005ebde5d379db46572a41e0
unknown <WilliamNdongmo88>
2025-02-23 00:40:48 +0100

    Registration et Login Done

---
+++ src/main/java/will/dev/appStore/configuration/SecurityConfig.java
@@ -0,0 +1,47 @@
+package will.dev.appStore.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import will.dev.appStore.service.UserService;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final UserService userService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf(csrf ->csrf.disable())
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/","/v1/login", "/v1/register","/user/{id}").permitAll()
+                                .anyRequest().authenticated())
+                /*.oauth2Login(oauth2Login -> oauth2Login // Nouvelle approche pour configurer OAuth2
+                        //.loginPage("/login") // Page de connexion personnalisée (optionnelle)
+                        .defaultSuccessUrl("/home") // URL de redirection après une connexion réussie
+                )*/
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http, PasswordEncoder passwordEncoder) throws Exception{
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userService).passwordEncoder(passwordEncoder);
+        return authenticationManagerBuilder.build();
+    }
+}
---
+++ src/main/java/will/dev/appStore/service/UserService.java
@@ -0,0 +1,28 @@
+package will.dev.appStore.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import will.dev.appStore.entites.User;
+import will.dev.appStore.repository.UserRepository;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if(user == null){
+            throw new UsernameNotFoundException("User not found with username" + username);
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority(user.getRole())));
+    }
+}
